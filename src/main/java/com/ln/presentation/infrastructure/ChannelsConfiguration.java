040fe3c0d07d7723c56b04a29ae165b686503274
Niecko, Lukasz <Lukasz.niecko.ctr@sabre.com>
2016-12-01 20:02:48 +0100

    Step 2.1 Custom source + asynchronous channel

--- src/main/java/com/ln/presentation/infrastructure/ChannelsConfiguration.java
+++ src/main/java/com/ln/presentation/infrastructure/ChannelsConfiguration.java
@@ -3,6 +3,9 @@ package com.ln.presentation.infrastructure;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.dsl.channel.MessageChannels;
+import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.scheduling.PollerMetadata;
 import org.springframework.messaging.MessageChannel;
 
 import static com.ln.presentation.infrastructure.Channels.CHANNEL_APPLICATION_INPUT;
@@ -11,9 +14,14 @@ import static com.ln.presentation.infrastructure.Channels.CHANNEL_CUSTOM_SOURCE_
 @Configuration
 public class ChannelsConfiguration {
 
+    @Bean(name = PollerMetadata.DEFAULT_POLLER)
+    public PollerMetadata poller() {
+        return Pollers.fixedRate(500).get();
+    }
+
     @Bean(name = CHANNEL_APPLICATION_INPUT)
     public MessageChannel channelApplicationInput() {
-        return new DirectChannel();
+        return MessageChannels.queue(100).get();
     }
 
     @Bean(name = CHANNEL_CUSTOM_SOURCE_INPUT)
